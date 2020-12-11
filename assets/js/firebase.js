d52b4c2f0dc572db08572247d7910572a00788e9
Ebuka <peterdbrainy5@gmail.com>
2020-12-11 13:27:36 +0100

    added new images and firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,20 +1,25 @@
 'use strict';
 
 //grab a form
-const form = document.querySelector('.form-inline');
+const form = document.querySelector('.main-form');
 
 //grab an input
 const inputEmail = form.querySelector('#inputEmail');
+const name = form.querySelector('#name');
+const subject = form.querySelector('#subject');
+const message = form.querySelector('#message');
+
 
 
 //config your firebase push
 const config = {
-    apiKey: "AIzaSyCfI_1234455glTdTNjFA3kAvmimlU",
-    authDomain: "example-9167e.firebaseapp.com",
-    databaseURL: "example-9167e.firebaseio.com",
-    projectId: "example-9167e",
-    storageBucket: "example-9167e.appspot.com",
-    messagingSenderId: "6271234586832"
+    apiKey: "AIzaSyDPH0q9SnFSAO8vG6GCeDPOYfkfykSpjuM",
+    authDomain: "design-cabal.firebaseapp.com",
+    databaseURL: "https://design-cabal.firebaseio.com",
+    projectId: "design-cabal",
+    storageBucket: "design-cabal.appspot.com",
+    messagingSenderId: "726472200802",
+    appId: "1:726472200802:web:77bebaabfce16d1b05373b",
 };
 
 
@@ -40,7 +45,8 @@ const config = {
     if (form) {
         form.addEventListener('submit', function (evt) {
             evt.preventDefault();
-            firebasePush(inputEmail);
+            firebasePush(inputEmail, name, subject, email);
+            
 
             //shows alert if everything went well.
             return alert('Data Successfully Sent to Realtime Database');
